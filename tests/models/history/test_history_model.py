a282d0507a6d584d1e29ec7649958c5de6558f03
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-08-18 10:42:54 -0300

    req. 7  ok

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,15 @@
-# import json
-# from src.models.history_model import HistoryModel
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    response = HistoryModel.list_as_json()
+    str1_line1 = '"text_to_translate": "Hello, I like videogame", '
+    str1_line2 = '"translate_from": "en", "translate_to": "pt"'
+    str1 = str1_line1 + str1_line2
+    assert str1 in response
+
+    str2_line1 = '"text_to_translate": "Do you love music?", '
+    str2_line2 = '"translate_from": "en", "translate_to": "pt"'
+    str2 = str2_line1 + str2_line2
+    assert str2 in response
