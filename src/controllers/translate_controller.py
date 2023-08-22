444b24f0757b53d7d6db9225accac4c3b5df9be6
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-08-22 11:56:02 -0300

    projeto concluído

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request
 from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 
-# from models.history_model import HistoryModel
+from models.history_model import HistoryModel
 
 
 translate_controller = Blueprint("translate_controller", __name__)
@@ -11,7 +11,7 @@ translate_controller = Blueprint("translate_controller", __name__)
 
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
-def index():
+def index(translate_from="pt", translate_to="en"):
     if request.method == "POST":
         text_to_translate = request.form["text-to-translate"]
         translate_from = request.form["translate-from"]
@@ -19,6 +19,13 @@ def index():
         translated = GoogleTranslator(
             source=translate_from, target=translate_to
         ).translate(text_to_translate)
+        HistoryModel(
+            {
+                "text_to_translate": text_to_translate,
+                "translate_from": translate_from,
+                "translate_to": translate_to,
+            }
+        ).save()
         return render_template(
             "index.html",
             languages=LanguageModel.list_dicts(),
@@ -30,6 +37,8 @@ def index():
     return render_template(
         "index.html",
         languages=LanguageModel.list_dicts(),
+        translated_from=translate_from,
+        translated_to=translate_to,
     )
 
 
@@ -50,3 +59,8 @@ def reverse():
         translated_to=translate_from,
         translated=translated,
     )
+
+
+@translate_controller.route("/aqui", methods=["GET"])
+def aqui():
+    return "aqui"
--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,29 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    # criar no banco um user admin com token
+    UserModel(
+        {"name": "Peter", "level": "admin", "token": "token_secreto123"}
+    ).save()
+    # criar um registro no histórico para ser deletado
+    HistoryModel(
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        }
+    ).save()
+
+    # busco o id de inserção do histórico para deleção
+    history_registry_id = HistoryModel.find_one({"translate_from": "en"}).data[
+        "_id"
+    ]
+
+    # fazer a requisição para deletar o registro
+    response = app_test.delete(
+        f"/admin/history/{history_registry_id}",
+        headers={"Authorization": "token_secreto123", "User": "Peter"},
+    )
+    assert response.status_code == 204
